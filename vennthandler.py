60657dd5bf8b645602d4c35f98a5195a8bf5894d
Josh Miller <miller.josh@husky.neu.edu>
2021-01-17 18:46:48 -0500

    test

--- vennthandler.py
+++ vennthandler.py
@@ -67,7 +67,6 @@ class VenntHandler(BaseHTTPRequestHandler):
 			self.send_header('Access-Control-Allow-Headers', '*')
 			self.send_header('Access-Control-Allow-Methods', 'OPTIONS, GET, HEAD, POST')
 			self.send_header('Allow', 'OPTIONS, GET, HEAD, POST')
-			self.send_header('Connection', 'keep-alive')
 			self.send_header('Content-type', 'text/html')
 			self.end_headers()
 			

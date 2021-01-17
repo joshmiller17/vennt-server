e2adbfacbf407be936dde6b743fcf9a8b74529f1
Josh Miller <miller.josh@husky.neu.edu>
2021-01-17 12:36:33 -0500

    Remove hardcode cert path

--- venntserver.py
+++ venntserver.py
@@ -13,16 +13,17 @@ PORT_NUMBER = 3004
 	
 
 if __name__ == '__main__':
-	if len(sys.argv) < 2 or len(sys.argv) > 3:
-		print('usage: %s [db] [optional: port number, default 3004]' % sys.argv[0])
+	if len(sys.argv) < 3 or len(sys.argv) > 4:
+		print('usage: %s [db] [certfile path] [optional: port number, default 3004]' % sys.argv[0])
 		sys.exit(-1)
 
 	db = sys.argv[1]
-	if len(sys.argv) == 3:
-		PORT_NUMBER = int(sys.argv[2])
+	cert = sys.argv[2]
+	if len(sys.argv) == 4:
+		PORT_NUMBER = int(sys.argv[3])
 	
 	httpd = HTTPServer((HOST_NAME, PORT_NUMBER), vennthandler.VenntHandler)
-	httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./server.pem', server_side=True)
+	httpd.socket = ssl.wrap_socket(httpd.socket, certfile=cert, server_side=True)
 	httpd.db = venntdb.VenntDB(db)
 
 	try:
